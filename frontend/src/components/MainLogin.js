b134dd66316c3f0385fbe6b1186687d748f2e2e4
Natali Siam <nataliesiam@yahoo.com>
2022-07-28 00:55:56 -0400

    ok now actually fixed the navbar

--- frontend/src/components/MainLogin.js
+++ frontend/src/components/MainLogin.js
@@ -8,7 +8,7 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import gamelogo from '../newgamelogo.png';
+import gamelogo from '../controllerlogo.png';
 import {FaBars, FaTimes} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
 import '../components/MainLogin.css';
@@ -46,8 +46,8 @@ function MainLogin()
         dynamicMain = 
         <div> 
         <span id="inner-title"></span><br />
-        <p>you ARE already logged in!</p>
-        <LoggedInName />
+        <p>Welcome,{<LoggedInName />}</p>
+        
         </div>
         
     }
@@ -56,73 +56,63 @@ function MainLogin()
         
         dynamicMain = 
         
-            <div className='header'>
-                {/* header/logo */}
-                <div className='logo'>
-                    <Link to='/'><img src={gamelogo} className='logo' alt="game logo"/></Link>
-                </div>
-                
-                {/* menu items */}
+            <div className={color ? 'header header-bg' : 'header'}>
+                <Link to='/'>
+                    <div className='logo'>
+                        <img src={gamelogo} className='logo' alt="game logo"/>
+                    </div>
+                </Link> 
                 <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                    {/* Link to home */}
                     <li className='btn btn-links'>
                         <Link to='/'>Home</Link>
                     </li>
-                    {/* Link to Games Menu which will take you to a page with the different consoles*/}
                     <li className='btn btn-links'>
                         <Link to='/games'>Games</Link>
                     </li>
-
-                    {/* link to My Games */}
                     <li className='btn btn-links'>
                         <Link to='/mylist'>My List</Link>
                     </li>
-
-                    {/* Link to About Us/Mission Statement */}
                     <li className='btn btn-links'>
                         <Link to='/about'>About Us</Link>
                     </li>
-
-                    {/* hamburger icon */}
-                    <div className='hamburger' onClick={handleClick}>
-                        {click ? (<FaTimes size={20} style={{color:'#fff'}}/>) : (<FaBars size={20} style={{color:'#fff'}}/>) }
                     
-                    </div>
-
-                    <Navbar  expand="lg">
-                    
-                        {/* <Container className='imgWrapper'>
-                            <a href="/" target="_blank" rel="noreferrer">
-                            <img src={gamelogo} className='logo' alt="game logo"/>
-                            </a>
-                        </Container> */}
-                        <Container className='loginWrapper'>
-                            <Navbar.Brand></Navbar.Brand>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="loginButtons">
-                                
-                                        <ModalComponent
-                                            buttonType ={"Login"}
-                                            title={"Login"}
-                                            body={""}
-                                            componentType={LoginModal}
-                                        />
-                                
-                                    <Navbar.Text style={{marginLeft: '.5rem'}}>{''}</Navbar.Text>
-                                        <ModalComponent
-                                            buttonType ={"Register"}
-                                            title={"Register"}
-                                            body={""}
-                                            componentType={RegisterModal}
-                                        />
-                                
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
-                
                 </ul>
+                <Navbar  expand="lg">
+                        
+                            <Container className='loginWrapper'>
+                                <Navbar.Brand></Navbar.Brand>
+                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                                <Navbar.Collapse id="basic-navbar-nav">
+                                    <Nav className="loginButtons">
+                                    
+                                            <ModalComponent
+                                                buttonType ={"Login"}
+                                                title={"Login"}
+                                                body={""}
+                                                componentType={LoginModal}
+                                            />
+                                    
+                                        <Navbar.Text style={{marginLeft: '.5rem'}}>{''}</Navbar.Text>
+                                            <ModalComponent
+                                                buttonType ={"Register"}
+                                                title={"Register"}
+                                                body={""}
+                                                componentType={RegisterModal}
+                                            />
+                                    
+                                    </Nav>
+                                </Navbar.Collapse>
+                            </Container>
+                        </Navbar>
+                    
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={20} style={{color: '#fff'}} />) : (<FaBars size={20} style={{color: '#fff'}} />)}
+                    
+                </div>
+            </div>
+
+                    
+                
 
                 
                 
@@ -158,7 +148,7 @@ function MainLogin()
                             </Navbar.Collapse>
                     </Container>
                 </Navbar> */} 
-            </div>
+            
         
         
     }
